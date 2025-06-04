0fca8ad2a92125b673baec8db40f4dbf033ce3d6
Gustavo Clemente <52931287+tatoclemente@users.noreply.github.com>
2025-06-04 11:33:44 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './redux/store';
 
 
 // axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://api-food-production-1e5f.up.railway.app'
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 
 ReactDOM.render(
   <Provider store={store}>
